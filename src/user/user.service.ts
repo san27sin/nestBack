d19637a3f2a8c10133aa5a42d0299da4a1dc981f
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2024-07-08 16:58:55 +0300

    lesson_04-07-2024

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { User } from '../types/User'
 
 @Injectable()
 export class UserService {
-	constructor(private readonly prismaService: PrismaService) {
-	
-	}
+	constructor(private readonly prismaService: PrismaService) { }
 	
 	create(user: Partial<User>) {
 		return this.prismaService.user.create({
 			data: { email: user.email, password: user.password, roles: ['USER'] }
 		});
 	}
-	
+
+	delete(id: number) {
+		return this.prismaService.user.delete({ id });
+	}
+
+	findById(id: number) {
+
+	}
+
+	findByEmail(email: string) {
+
+	}
+
 	// TODO удалять / находить по id или email /
 }
